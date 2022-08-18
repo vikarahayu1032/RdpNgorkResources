d39e7814fe411da9badde1c518c6422adcc723e3
vikarahayu1032 <111042349+vikarahayu1032@users.noreply.github.com>
2022-08-18 13:32:30 +0700

    Update task_4.bat

--- task_4.bat
+++ task_4.bat
@@ -1,4 +1,4 @@
-TITLE SYNC DRAKOR URL 1 OF 6 TO DRIVE 1
+TITLE SYNC DRAKOR URL 4 TO DRIVE 1
 cd C:\RcloneResources
 rclone sync drakor_url4: drive_me1:MOVIE_DRAKOR_WHATBOX/URL_4 --error-on-no-transfer --transfers 1 --progress
 PAUSE
