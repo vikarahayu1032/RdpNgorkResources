96f890cf7f5c9c20a3824efca00cb4a7d3abb854
vikarahayu1032 <111042349+vikarahayu1032@users.noreply.github.com>
2022-08-18 13:31:09 +0700

    Create task_2.bat

---
+++ task_2.bat
@@ -0,0 +1,5 @@
+TITLE SYNC DRAKOR URL 1 OF 6 TO DRIVE 1
+cd C:\RcloneResources
+rclone sync drakor_url2: drive_me1:MOVIE_DRAKOR_WHATBOX/URL_2 --error-on-no-transfer --transfers 1 --progress
+PAUSE
+
